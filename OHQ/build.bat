9d1934b2e139011fc73070267a8b5df01d3fafc0
JoakimIsraelsson <JoakimIsraelsson@fbf1ad31-3caa-4afa-b5b4-094426959cba>
2008-06-27 22:08:21 +0000

    Added some veery basic automated build capability. Note that the framework filepath must be in the PATH system variable for this to work.
    git-svn-id: http://svn.assembla.com/svn/100_Quests@20 fbf1ad31-3caa-4afa-b5b4-094426959cba

---
+++ OHQ/build.bat
@@ -0,0 +1,14 @@
+@echo off
+
+REM Currently the build fails on the first build but succeeds
+REM on the second. I assume that something is built in the
+REM wrong order.
+msbuild.exe OHQ.sln /p:Configuration=Release
+msbuild.exe OHQ.sln /p:Configuration=Release
+msbuild.exe OHQ.sln /p:Configuration=Debug
+msbuild.exe OHQ.sln /p:Configuration=Debug
+
+cd OHQ\bin\x86
+..\..\..\zip ..\..\..\Release.zip -r Release\*
+..\..\..\zip ..\..\..\Debug.zip -r Debug\*
+cd ..\..\..
