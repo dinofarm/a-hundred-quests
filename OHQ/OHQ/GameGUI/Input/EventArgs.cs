777f12c4090ae5f9669277c7dd65ae75a5e9d13f
Joshua Jackson <seekndestroyer@hotmail.com>
2008-12-10 20:08:27 -0500

    MouseEvents started

---
+++ OHQ/OHQ/GameGUI/Input/EventArgs.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace OHQ.GameGUI.Input
+{
+    #region Mouse
+    public enum MouseButton
+    {
+        Left, Right, Middle,
+        Button4, Button5
+    }
+
+    public class MouseEventArgs : System.EventArgs
+    {
+        #region Properties
+
+        public MouseState State { get; set; }
+
+        public MouseButton MouseButton { get; set; }
+
+        public Point Posistion { get; set; }
+
+        #endregion
+    }
+
+    public delegate void OnMouseButtonDownEventHandler(object sender, MouseEventArgs e);
+    public delegate void OnMouseButtonUpEventHandler(object sender, MouseEventArgs e);
+    #endregion
+}
