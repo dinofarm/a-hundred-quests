6f933a976a6ca0c9b4b180af4de7de7b074bb328
unknown <Administrator@.(none)>
2008-12-12 18:00:39 -0500

    EventArgs and Enums finished

--- OHQ/OHQ/GameGUI/Input/EventArgs.cs
+++ OHQ/OHQ/GameGUI/Input/EventArgs.cs
@@ -3,27 +3,69 @@ using Microsoft.Xna.Framework.Input;
 
 namespace OHQ.GameGUI.Input
 {
-    #region Mouse
+    public enum Player
+    {
+        One = 1,
+        Two = 2,
+        Three = 4,
+        Four = 8
+    }
+    public enum GamepadButton
+    {
+        A, B, X, Y,
+        RightShoulder, LeftShoulder,
+        RightStick, LeftStick,
+        LeftStickUp, LeftStickDown,
+        LeftStickLeft, LeftStickRight,
+        RightStickUp, RightStickDown,
+        RightStickLeft, RightStickRight,
+        LeftTrigger, RightTrigger,
+        DpadUp, DpadDown, DpadLeft, DpadRight,
+        Back, Start, BigButton
+    }
     public enum MouseButton
     {
-        Left, Right, Middle,
-        Button4, Button5
+        Left, Right, Middle, Button4, Button5
     }
-
+    public class CancelEventArgs : System.EventArgs
+    {
+        #region Properties
+        bool Canceled { get; set; }
+        #endregion
+    }
+    public class GamepadEventArgs : System.EventArgs
+    {
+        #region Properties
+        GamePadState GpState { get; set; }
+        GamepadButton GpButton { get; set; }
+        GamePadDPad GpDPad { get; set; }
+        
+        private PlayerIndex m_PlayerIndex = PlayerIndex.One;
+        public PlayerIndex PlayerIndex
+        {
+            get { return m_PlayerIndex; }
+            set { m_PlayerIndex = value; }
+        }
+        #endregion
+    }
     public class MouseEventArgs : System.EventArgs
     {
         #region Properties
 
         public MouseState State { get; set; }
-
         public MouseButton MouseButton { get; set; }
-
         public Point Posistion { get; set; }
 
         #endregion
     }
 
-    public delegate void OnMouseButtonDownEventHandler(object sender, MouseEventArgs e);
-    public delegate void OnMouseButtonUpEventHandler(object sender, MouseEventArgs e);
-    #endregion
+    public class KeyboardEventArgs : System.EventArgs
+    {
+        #region Properties
+        public Keys Key { get; set; }
+        public bool Alt { get; set; }
+        public bool Ctrl { get; set; }
+        public bool Shift { get; set; }
+        #endregion
+    }
 }
---
+++ OHQ/OHQ/GameGUI/Input/EventBasedInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+namespace OHQ.GameGUI.Input
+{
+    #region Delegates
+
+    public delegate void OnMouseButtonDownEventHandler(object sender, MouseEventArgs e);
+    public delegate void OnMouseButtonUpEventHandler(object sender, MouseEventArgs e);
+
+    public delegate void OnKBKeyDownEventHandler(object sender, KeyboardEventArgs e);
+    public delegate void OnKBKeyUpEventHandler(object sender, KeyboardEventArgs e);
+
+
+    #endregion
+
+    public interface IEventBasedInput
+    {
+
+    }
+}
